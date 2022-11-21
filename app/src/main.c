746ae73d5d7815e090198a435db1f25c9839efe3
Francois Gervais <francoisgervais@gmail.com>
2022-11-20 22:21:24 -0500

    Monitor the battery voltage

--- app/src/main.c
+++ app/src/main.c
@@ -1,3 +1,4 @@
+#include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/kernel.h>
@@ -18,10 +19,20 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct pwm_dt_spec buzzer = PWM_DT_SPEC_GET(DT_PATH(buzzer, pwm));
+static const struct adc_dt_spec battery_adc = ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 0);
 
 void main(void)
 {
 	int ret;
+	int err;
+	int16_t buf;
+
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 
@@ -39,13 +50,56 @@ void main(void)
 		printk("%s: device not ready.\n", buzzer.dev->name);
 		return;
 	}
+	if (!device_is_ready(battery_adc.dev)) {
+		printk("ADC controller device not ready\n");
+		return;
+	}
+
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
 	}
 
+	err = adc_channel_setup_dt(&battery_adc);
+	if (err < 0) {
+		printk("Could not setup battery ADC (%d)\n", err);
+		return;
+	}
+
 	pwm_set_dt(&buzzer, PWM_USEC(250U), PWM_USEC(250U) * 0.53);
+
+	while (1) {
+		int32_t val_mv;
+
+		printk("ADC reading:\n");
+
+		printk("- %s, channel %d: ",
+		       battery_adc.dev->name,
+		       battery_adc.channel_id);
+
+		(void)adc_sequence_init_dt(&battery_adc, &sequence);
+
+		err = adc_read(battery_adc.dev, &sequence);
+		if (err < 0) {
+			printk("Could not read (%d)\n", err);
+			continue;
+		} else {
+			printk("%"PRId16, buf);
+		}
+
+		/* conversion to mV may not be supported, skip if not */
+		val_mv = buf;
+		err = adc_raw_to_millivolts_dt(&battery_adc,
+					       &val_mv);
+		if (err < 0) {
+			printk(" (value in mV not available)\n");
+		} else {
+			printk(" = %"PRId32" mV\n", val_mv);
+		}
+
+		k_sleep(K_MSEC(1000));
+	}
 
 	LOG_INF("****************************************");
 	LOG_INF("MAIN DONE");
